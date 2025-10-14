3d98c3cca98cb4f9789cac04e2d1e8944cc48721
danildperez04 <gamerdan589@gmail.com>
2025-10-14 15:48:35 -0600

    fix: add duplicated property error handler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,12 +1,30 @@
+import { DatabaseError } from 'pg';
 import { HttpException, ValidationException } from '../common/exceptions/httpException';
 import { NextFunction, Request, Response } from 'express';
+import { QueryFailedError } from 'typeorm';
+import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
+import { HttpStatusCode } from '../common/http/httpStatusCode';
 
 export function errorHandler(err: Error, req: Request, res: Response, _next: NextFunction) {
   console.error(err);
 
-  let statusCode = 500;
+  const success = false;
+  let statusCode = HttpStatusCode.INTERNAL_SERVER_ERROR;
   let message = 'Internal Server Error';
   let errors;
+
+  if (err instanceof QueryFailedError) {
+    message = 'Bad request';
+    statusCode = HttpStatusCode.BAD_REQUEST;
+
+    const error = err.driverError as DatabaseError;
+
+    if (error.code === '23505') {
+      message = error?.detail?.replace(
+        /^Key \((.*)\)=\((.*)\) (.*)/,
+        'This $1 already exists.') || message;
+    }
+  }
 
   if (err instanceof ValidationException) {
     const details = err.details;
@@ -14,11 +32,24 @@ export function errorHandler(err: Error, req: Request, res: Response, _next: Nex
     errors = details.map(({ constraints }) => constraints);
   }
 
+  if (err instanceof JsonWebTokenError) {
+    statusCode = HttpStatusCode.UNAUTHORIZED;
+    message = 'Login to access';
+  }
+
+  if (err instanceof TokenExpiredError) {
+    statusCode = HttpStatusCode.UNAUTHORIZED;
+    message = 'The sesion expired';
+  }
+
   if (err instanceof HttpException) {
     statusCode = err.status;
     message = err.message;
   }
 
-
-  res.status(statusCode).json({ message, errors });
+  res.status(statusCode).json({
+    success,
+    message,
+    errors
+  });
 }
