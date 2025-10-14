e334a6510cd30677719251777ab89db1e3fa9d3c
danildperez04 <gamerdan589@gmail.com>
2025-10-14 07:40:55 -0600

    feat: add error and not found middlewares

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Express } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import router from './routes';
+import { notFoundHandler } from 'middlewares/notfound';
+import { errorHandler } from 'middlewares/errorHandler';
 
 const app: Express = express();
 
@@ -18,7 +20,9 @@ app.get('/', (req, res) => {
 app.use('/api', router);
 
 // Use the notFoundHandler middleware
+app.use(notFoundHandler);
 
 // Error handling
+app.use(errorHandler);
 
 export default app;
---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,24 @@
+import { ValidationError } from 'class-validator';
+import { HttpException } from '../common/exceptions/httpException';
+import { NextFunction, Request, Response } from 'express';
+
+export function errorHandler(err: Error, req: Request, res: Response, _next: NextFunction) {
+  console.error(err);
+
+  let statusCode = 500;
+  let message = 'Internal Server Error';
+  let errors;
+
+  if (err instanceof HttpException) {
+    statusCode = err.status;
+    message = err.message;
+  }
+
+  if (typeof err === 'object' && err !== null && 'details' in err) {
+    const details = (err as { details: ValidationError[] }).details;
+
+    errors = details.map(({ constraints }) => constraints);
+  }
+
+  res.status(statusCode).json({ message, errors });
+}
