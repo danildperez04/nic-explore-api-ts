4b400f4de5ed720d38a93286c5e8f7761cd07b73
danildperez04 <gamerdan589@gmail.com>
2025-10-14 14:48:55 -0600

    add Interfaces for service and repository

---
+++ src/services/IService.ts
@@ -0,0 +1,7 @@
+export interface IService<T> {
+  findAll(): Promise<T[]>;
+  findOne(id: number): Promise<T | null>;
+  create(data: Partial<T>): Promise<T>;
+  update(id: number, data: Partial<T>): Promise<T | null>;
+  remove(id: number): Promise<void>;
+}
